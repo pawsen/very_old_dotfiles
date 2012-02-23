89558309e85ecada6af4792557051d16787eec77
Paw Møller <pawsen@gmail.com>
2012-02-24 00:49:20 +0100

    updated xbindkeys to use snap

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -499,7 +499,7 @@ function run_once(prg,arg_string,pname,screen)
 end
 
 --run_once("wicd-client",nil,"/usr/bin/python2 -O /usr/share/wicd/gtk/wicd-client.py")
-run_once("wicd-client","--tray &",nil)
+run_once("wicd-client","--tray &",'nil')
 run_once("devmon","&",nil)
 run_once("volti","&",nil)
 run_once("xfce4-power-manager","&",nil)
